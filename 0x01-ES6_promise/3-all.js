e3f17da9a3efba0c086497026d4a1553f1c3993c
Najlae86 <najlae.aissaoui86@gmail.com>
2024-10-02 21:31:07 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,10 @@ import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user) => {
-      console.log(photo.body, user.firstName, user.lastName);
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
