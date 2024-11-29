31f44535a0c67a5eeedba1c90b4c2cca3eb81f72
Najlae86 <najlae.aissaoui86@gmail.com>
2024-11-29 10:37:09 -0800

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * displays a message on the STDOUT.
+ * @param {String} msg the message to display.
+ */
+const displayMessage = (msg) => {
+	console.log(msg);
+};
+
+module.exports = displayMessage;
